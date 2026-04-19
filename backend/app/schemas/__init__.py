8bcb408090df7b32d7ec5219f05bd72ead0e5794
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-04-19 07:40:12 +0300

    Create __init__.py

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,13 @@
+from app.schemas.team_builder import (
+    RecommendRequest,
+    RecommendResponse,
+    SavedProfileCreate,
+    SavedProfileResponse,
+)
+
+__all__ = [
+    "RecommendRequest",
+    "RecommendResponse",
+    "SavedProfileCreate",
+    "SavedProfileResponse",
+]
